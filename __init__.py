9cb6bec2f7933515c2b278f24d4cbbed68126af3
Aalind Sharma <aalind.sharma@st.niituniversity.in>
2018-12-19 15:51:36 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -13,11 +13,12 @@ bcrypt = Bcrypt(app) #HASHED PASSWORD
 login_manager = LoginManager(app) #FOR LOGIN AUTHENTICATION
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = 'aalind84@gmail.com'#os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = '11071997#Aadi'#os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 from flaskblog import routes
--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed #TO UPLOAD PROFILE PICTURE IN ACCOUNT PAGE
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskblog.models import User
+from flaskblog.models import User, Subscriber
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -56,21 +56,20 @@ class UpdateAccountForm(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    category = SelectField(u'Pick category:',
+                                choices=[('sports','Sports'),('entertainment','Entertainment'),('arts','Arts'),
+                                ('business','Business'),('science','Science'),('psychology','Psychology'),
+                                ('health','Health'),('technology','Technology'),('politics','Politics'),
+                                ('travel','Travel'),('fashion','Fashion')])
+    picture = FileField('Add Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Post')
 
-class RequestResetForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
+class SubscribeForm(FlaskForm):
+    poster_id = StringField('Poster_Id', validators=[DataRequired()])
+    poster_name = StringField('Poster_Name', validators=[DataRequired()])
+    follow_unfollow = BooleanField('')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is None:
-            raise ValidationError('There is no account with that email. You must register first.')
-
-
-class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset Password')
+class FollowingForm(FlaskForm):
+    poster_id = StringField('Poster_Id', validators=[DataRequired()])
+    poster_name = StringField('Poster_Name', validators=[DataRequired()])
+    follow_unfollow = BooleanField('')
