14580371868b5469e453708ac1cef00d524084bc
Nandini7107 <44194911+Nandini7107@users.noreply.github.com>
2018-12-20 23:07:01 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -14,23 +14,21 @@ class User(db.Model, UserMixin): #TO CREATE USER MODEL
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True) 
+    posts = db.relationship('Post', backref='author', lazy=True)
+    subscribers = db.relationship('Subscriber', backref='poster', lazy=True)
     #POST ATTRIBUTE HAS RELATIONSHIP TO POST MODEL
     #BACKREF IS SIMILAR TO ADDING ANOTHER COLUMN TO POST MODEL
     #USING BACKREF WHEN WE HAVE ANY POST WE CAN USE THIS AUTHOR ATTRIBUTE TO GET USER WHO CREATED THE POST
-    
-    def get_reset_token(self, expires_sec=1800): # FOR PASSWORD RESET
+    def get_reset_token(self, expires_sec=1800):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
-    @staticmethod
     def verify_reset_token(token):
         s = Serializer(app.config['SECRET_KEY'])
         try:
             user_id = s.loads(token)['user_id']
         except:
-            return None
-        return User.query.get(user_id)
+            return None 
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -40,8 +38,20 @@ class Post(db.Model): #TO CREATE POST MODEL
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) #UTCNOW IS USED TO SAVE DATE AND TIME
+    image_file = db.Column(db.String(20))
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category = db.Column(db.String(20), nullable=False)
     #IN FOREIGN KEY WE ARE REFERENCING TABLE NAME AND COLUMN NAME WHEREAS IN POST RELATIONSHIP WE ARE GIVING POST MODEL
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+class Subscriber(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    poster_id = db.Column(db.Integer, nullable=False)
+    poster_name = db.Column(db.String(20), nullable=False)
+    subscriber_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    subscriber_name = db.Column(db.String(20), nullable=False)
+    
+    def __repr__(self):
+        return f"Subscribe('{self.poster_name}', '{self.subscriber_name}')" 
--- routes.py
+++ routes.py
@@ -3,19 +3,27 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt, mail
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
-from flaskblog.models import User, Post
+from flaskblog.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
+								 PostForm, SubscribeForm, FollowingForm)
+from flaskblog.models import User, Post, Subscriber
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
 #WE CAN REPRESENT DATABASE STRUCTURE AS CLASSES OR CALLED MODELS. THESE CAN BE PUT INTO DIFFERENT FILE
 
+#--------------------------------------------HOME PAGE ROUTE--------------------------------------------------
 
 @app.route("/")
+
 @app.route("/home")
+@login_required
 def home():
-	page = request.args.get('page', 1, type=int)
-	posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2) #PAGINATE ALL POSTS. PER PAGE IS 5
-	return render_template('home.html', posts=posts)
+	posts = Post.query.order_by(Post.date_posted.desc())
+	subscribee = Subscriber.query.filter_by(subscriber_id=current_user.id)
+	subs = []
+	for x in subscribee:
+		subs.append(x.poster_id)
+
+	return render_template('home.html', posts=posts, current_user=current_user,subs=subs)
 
 
 @app.route("/about")
@@ -37,6 +45,14 @@ def register():
 		return redirect(url_for('login')) #AFTER SUCCESSFUL ACCOUNT CREATION USER IS REDIRECTED TO LOGIN PAGE
 	return render_template('register.html', title='Register', form=form)
 
+#--------------------------------------------USER MY POSTS ROUTE----------------------------------------------
+@app.route("/myposts", methods=['GET', 'POST'])
+def my_posts():
+	
+	posts = Post.query.filter_by(user_id=current_user.id).order_by(Post.date_posted.desc())
+	return render_template('my_posts.html', title='My Posts', posts=posts)
+
+#--------------------------------------------LOGIN ROUTE----------------------------------------------
 
 @app.route("/login", methods=['GET', 'POST']) #WE NEED TO ACCEPT GET/POST REQUEST OTHER IT WILL THROW METHOD NOT FOUND ERROR
 def login():
@@ -53,10 +69,14 @@ def login():
 			flash('Login Unsuccessful. Please check email and password', 'danger') #THIS MESSAGE GETS TO LAYOUT PAGE
 	return render_template('login.html', title='Login', form=form)
 
+#--------------------------------------------LOGOUT ROUTE----------------------------------------------
+
 @app.route("/logout")
 def logout():
 	logout_user()
-	return redirect(url_for('home'))
+	return redirect(url_for('login'))
+
+#--------------------------------------------SAVE PICTURE FUNCTION----------------------------------------------
 
 def save_picture(form_picture):
 	random_hex = secrets.token_hex(8)
@@ -69,6 +89,8 @@ def save_picture(form_picture):
 	i.save(picture_path)
 
 	return picture_fn
+
+#--------------------------------------------ACCOUNT ROUTE----------------------------------------------
 
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
@@ -89,44 +111,90 @@ def account():
 	image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
 	return render_template('account.html', title='Account', image_file=image_file, form=form)
 
+#--------------------------------------------NEW POST ROUTE----------------------------------------------
+
+def post_picture(form_picture):
+	random_hex = secrets.token_hex(8)
+	_, f_ext = os.path.splitext(form_picture.filename)
+	picture_fn = random_hex + f_ext
+	picture_path = os.path.join(app.root_path, 'static/post_pics', picture_fn)
+	
+	i = Image.open(form_picture)
+	i.save(picture_path)
+
+	return picture_fn
+
+def send_post_email(user,post_id,subs):
+	msg = Message(f'New Post by {user.username}',sender='noreply@gmail.com',recipients=subs)
+	msg.body = f'''To see the new post, visit the following link:
+{url_for('post', post_id=post_id, _external=True)}
+'''
+	mail.send(msg)
+
 @app.route("/post/new", methods=['GET', 'POST']) #FOR POSTS BY USER IN BLOG
 @login_required #BECAUSE CREATING POST REQUIRES USER TO LOGIN
 def new_post():
 	form = PostForm()
 	if form.validate_on_submit():
-		post = Post(title=form.title.data, content=form.content.data, author=current_user)
+		if form.picture.data:
+			picture_file = post_picture(form.picture.data)
+		else:
+			picture_file = None
+			
+		post = Post(title=form.title.data,content=form.content.data,
+					author=current_user,image_file=picture_file,category=form.category.data)
 		db.session.add(post)
 		db.session.commit()
 		flash('Your post has been created!', 'success')
+		
+		post = Post.query.filter_by(title=form.title.data,content=form.content.data,author=current_user,category=form.category.data).first()
+		post_id = post.id
+		subscribers = Subscriber.query.filter_by(poster_id=current_user.id,poster_name=current_user.username)
+		subs = []
+		for x in subscribers:
+			subs.append(x.poster.email)
+		if subs:
+			send_post_email(current_user, post_id, subs)
 		return redirect(url_for('home'))
+
 	return render_template('create_post.html', title='New Post',
                            form=form, legend='New Post')
+		
+#--------------------------------------------POST ROUTE-----------------------------------------------------
 
 @app.route("/post/<int:post_id>")
+@login_required
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
+#--------------------------------------------UPDATE POST ROUTE----------------------------------------------
 
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    if post.author != current_user:
-        abort(403)
-    form = PostForm()
-    if form.validate_on_submit():
-        post.title = form.title.data
-        post.content = form.content.data
-        db.session.commit()
-        flash('Your post has been updated!', 'success')
-        return redirect(url_for('post', post_id=post.id))
-    elif request.method == 'GET':
-        form.title.data = post.title
-        form.content.data = post.content
-    return render_template('create_post.html', title='Update Post',
+	post = Post.query.get_or_404(post_id)
+	if post.author != current_user:
+		abort(403)
+	form = PostForm()
+	if form.validate_on_submit():
+		if form.picture.data:
+			picture_file = post_picture(form.picture.data)
+			post.image_file = picture_file
+		post.title = form.title.data
+		post.content = form.content.data
+		post.category = form.category.data
+		db.session.commit()
+		flash('Your post has been updated!', 'success')
+		return redirect(url_for('post', post_id=post.id))
+	elif request.method == 'GET':
+		form.title.data = post.title
+		form.content.data = post.content
+		form.category.data = post.category
+	return render_template('create_post.html', title='Update Post',
                            form=form, legend='Update Post')
 
+#--------------------------------------------DELETE POST ROUTE----------------------------------------------
 
 @app.route("/post/<int:post_id>/delete", methods=['POST'])
 @login_required
@@ -139,54 +207,98 @@ def delete_post(post_id):
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
 
-@app.route("/user/<string:username>")
+#--------------------------------------------USER ROUTE----------------------------------------------
+
+@app.route("/user/<string:username>") # GIVES POSTS BY USER BY CLICKING ON USER NAME
 def user_posts(username):
-	page = request.args.get('page', 1, type=int)
+	
 	user = User.query.filter_by(username=username).first_or_404()
 	posts = Post.query.filter_by(author=user)\
-			.order_by(Post.date_posted.desc())\
-			.paginate(page=page, per_page=2) #PAGINATE ALL POSTS. PER PAGE IS 5
+			.order_by(Post.date_posted.desc())
 	return render_template('user_posts.html', posts=posts, user=user)
 
-def send_reset_email(user):
-    token = user.get_reset_token()
-    msg = Message('Password Reset Request',
-                  sender='noreply@demo.com',
-                  recipients=[user.email])
-    msg.body = f'''To reset your password, visit the following link:
-{url_for('reset_token', token=token, _external=True)}
+#--------------------------------------------SUBSCRIBER ROUTE--------------------------------------------------
 
-If you did not make this request then simply ignore this email and no changes will be made.
-'''
-    mail.send(msg)
+@app.route("/subscribe", methods=['GET', 'POST'])
+@login_required
+def subscribe():
+	form = SubscribeForm()
+	users = User.query.order_by(User.id.asc())
+	subscribee = Subscriber.query.filter_by(subscriber_id=current_user.id)
+	subs = []
+	for x in subscribee:
+		subs.append(x.poster_id)
 
+	poster_id = request.args.get('poster_id', default=None)
+	poster_name = request.args.get('poster_name', default=None)
+	subscribe_checkbox = form.follow_unfollow.data
 
-@app.route("/reset_password", methods=['GET', 'POST'])
-def reset_request():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RequestResetForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        send_reset_email(user)
-        flash('An email has been sent with instructions to reset your password.', 'info')
-        return redirect(url_for('login'))
-    return render_template('reset_request.html', title='Reset Password', form=form)
+	if subscribe_checkbox == True:
+		subscriber = Subscriber(subscriber_id=current_user.id,subscriber_name=current_user.username,poster_id=poster_id,poster_name=poster_name)
+		db.session.add(subscriber)
+		db.session.commit()
+		flash(f'You have been subscribed to {poster_name}', 'success')
+		return redirect(url_for('home'))
+			
+	return render_template('subscribe.html',title='Subscribe',users=users,current_user=current_user,form=form,subs=subs)
 
+#--------------------------------------------FOLLOWING ROUTE--------------------------------------------------
 
-@app.route("/reset_password/<token>", methods=['GET', 'POST'])
-def reset_token(token):
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    user = User.verify_reset_token(token)
-    if user is None:
-        flash('That is an invalid or expired token', 'warning')
-        return redirect(url_for('reset_request'))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user.password = hashed_password
-        db.session.commit()
-        flash('Your password has been updated! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('reset_token.html', title='Reset Password', form=form)
+@app.route("/following", methods=['GET', 'POST'])
+@login_required
+def following():
+	form = FollowingForm(follow_unfollow=True)
+	users = User.query.order_by(User.id.asc())
+	followings = Subscriber.query.filter_by(subscriber_id=current_user.id)
+	subs = []
+	for x in followings:
+		subs.append(x.poster_id)
+	
+	poster_id = request.args.get('poster_id', default=None)
+	poster_name = request.args.get('poster_name', default=None)
+	subscribe_checkbox = form.follow_unfollow.data
+
+	if subscribe_checkbox == False:
+		following = Subscriber.query.filter_by(subscriber_id=current_user.id,subscriber_name=current_user.username,poster_id=poster_id,poster_name=poster_name).first()
+		db.session.delete(following)
+		db.session.commit()
+		flash(f'You have been unsubscribed to {poster_name}', 'success')
+		return redirect(url_for('home'))
+	
+	return render_template('following.html',title='Following',users=users,current_user=current_user,form=form,subs=subs)
+
+#--------------------------------------------FOLLOWERS ROUTE--------------------------------------------------
+
+@app.route("/followers", methods=['GET', 'POST'])
+@login_required
+def followers():
+	
+	users = User.query.order_by(User.id.asc())
+	followings = Subscriber.query.filter_by(poster_id=current_user.id)
+	subs = []
+	for x in followings:
+		subs.append(x.subscriber_id)
+	
+	return render_template('followers.html',title='Followers',users=users,current_user=current_user,subs=subs)
+
+#--------------------------------------------CATEGORY ROUTE--------------------------------------------------
+
+@app.route("/category/<category_type>", methods=['GET', 'POST'])
+@login_required
+def category(category_type):
+	posts = Post.query.filter_by(category=category_type).order_by(Post.date_posted.desc())
+	followings = Subscriber.query.filter_by(subscriber_id=current_user.id)
+	subs = []
+	for x in followings:
+		subs.append(x.poster_id)
+	
+	poster_id = request.args.get('poster_id', default=None)
+	post_id = request.args.get('post_id', default=None)
+	
+	if poster_id:
+		if int(poster_id) in subs:
+			return redirect(url_for('post', post_id=post_id))
+		else:
+			return redirect(url_for('subscribe'))
+	
+	return render_template('category.html', title=category_type, posts=posts, current_user=current_user)
